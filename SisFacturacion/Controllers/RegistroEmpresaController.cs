be1f6ac584babf80ae0219227a2334e661071f49
CONTRALORIA\aalba <aalba@contraloria.gob.do>
2023-02-09 14:54:31 -0400

    Agregando Estilos, Plugins, images, etc.

---
+++ SisFacturacion/Controllers/RegistroEmpresaController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SisFacturacion.Controllers
+{
+    public class RegistroEmpresaController : Controller
+    {
+        // GET: RegistroEmpresa
+        public ActionResult RegistroEmpresa()
+        {
+            return View();
+        }
+
+        // GET: RegistroEmpresa/Details/5
+      
+    }
+}

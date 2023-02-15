f76ebcb2e617e6e6d1b47960d806507b443cf2b7
AlbertARey <124914447+AlbertARey@users.noreply.github.com>
2023-02-14 21:57:10 -0400

    formulario de registroempresa finish y funcion de visualizar ready

---
+++ Scripts/App/RegistroEmpresa.js
@@ -0,0 +1,23 @@
+﻿/*Visualizar imagen*/
+
+$("#imagen").change(function () {
+    let imagen = this.files[0];
+
+    if (imagen["type"] != "image/jpeg" && imagen["type"] != "image/png") {
+        $("#imagen").val("");
+        $(".previsualizar").attr("src", "../Content/Image/img_logo/TuLogo.png");
+        alert("Debe subir una Imagen en Formato JPEG o PNG")
+    } else if (imagen["size"] > 2000000) {
+        $("#imagen").val("");
+        $(".previsualizar").attr("src", "../Content/Image/img_logo/TuLogo.png");
+        alert("La imagen no puede exceder los 2MB")
+    } else {
+        var datosImagen = new FileReader;
+        datosImagen.readAsDataURL(imagen);
+
+        $(datosImagen).on("load", function (event) {
+            var rutaImagen = event.target.result;
+            $(".previsualizar").attr("src", rutaImagen);
+        })
+    }
+})
